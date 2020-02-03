80355b94afad07293a35b8874bbe3326b256dda6
KlucsikKP <klucsik.krisztian@gmail.com>
2020-02-03 22:59:36 +0100

    v0_4 http set to insecure, update_interval added to cinfig, timeoutinterval added to config

---
+++ src/main.ino
@@ -0,0 +1,454 @@
+static String name = "noszlop_kijelzo"; //to csiraztato
+static String ver = "0_4"; //diff to 0_3: alarm kiírja a message-t is, https client insecure 
+
+//////////////////////////////////////////////
+////////////CONFIG////////////////////////////
+#include "secrets.h"
+Secrets sec;
+const char* csir_homerseklet;
+int csir_last_on;
+int network_timeout=0;
+int  csir_timeout=3;
+int pinginterval=1;
+int update_interval=5;
+const uint8_t Googlefingerprint[20] = {0x63, 0x92, 0xD6, 0x31, 0x89, 0x30, 0x9B, 0x7A, 0x94, 0x33, 0x71, 0x67, 0xB5, 0x9F, 0xDE, 0x99, 0x69, 0xA1, 0x88, 0xF7};
+const uint8_t Discordfingerprint[20] = {0x2D, 0x08, 0xE9, 0x2D, 0x0A, 0x54, 0x5A, 0xD5, 0xB4, 0x0A, 0x57, 0xF6, 0x68, 0x85, 0x1A, 0x79, 0x52, 0xE1, 0xFA, 0x65};
+const String update_server = sec.update_server;
+#define USE_SERIAL Serial
+
+const String GScriptId = sec.gID;
+const String discord_chanel = sec.discord_chanel;
+
+////////////CONFIG////////////////////////////
+//////////////////////////////////////////////
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <ESP8266httpUpdate.h>
+#include <ESP8266HTTPClient.h>
+#include <TimeLib.h>
+#include <ArduinoJson.h>
+#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+ESP8266WiFiMulti WiFiMulti;
+WiFiClient client;
+
+#include <LiquidCrystal_PCF8574.h>
+#include <Wire.h>
+
+LiquidCrystal_PCF8574 lcd(0x27); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+
+//////////////////////////////////////////////
+////////////SETUP ////////////////////////////
+void setup()
+{
+  //test_lcd
+  lcd.begin(20, 4);
+  lcd.setBacklight(255);
+  lcd.home();
+  lcd.clear();
+  lcd.print("Hello LCD");
+  delay(1000);
+  lcd.setBacklight(0);
+  delay(400);
+  lcd.setBacklight(255);
+
+
+  USE_SERIAL.begin(115200);
+  USE_SERIAL.setDebugOutput(true);
+  USE_SERIAL.println();
+  USE_SERIAL.print("name: ");
+  USE_SERIAL.println(name);
+  USE_SERIAL.print("ver: ");
+  USE_SERIAL.println(ver);
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("mocsigoncska_ap");
+  Serial.println("connected...yeey :)");
+  delay(1000);
+
+  GsheetPost(F("log"), "startup: " + name + " " + ver);
+  discordPost("startup: " + name + " " + ver);
+
+  updateFunc(name, ver);
+
+}
+
+////////////SETUP ////////////////////////////
+//////////////////////////////////////////////
+
+//////////////////////////////////////////////
+////////////LOOP ////////////////////////////
+int i = 0;
+int j = 0;
+void loop()
+{
+  Serial.println("loop...");
+  delay(1000 * pinginterval);
+  getdata();
+  getconfig();
+  kijelzo();
+  Serial.print("----last on: ");
+  Serial.println(csir_last_on);
+  if(csir_last_on > csir_timeout){
+    alarm("nem latom a csiraztatot");
+  }
+
+  j++;
+  if (j > update_interval)
+  {
+    j = 0;
+    updateFunc(name, ver);
+  }
+  if(network_timeout>5) alarm("net hiba");
+}
+
+////////////LOOP ////////////////////////////
+//////////////////////////////////////////////
+
+/////////////////////////////////////////////
+////////////KIJELZO//////////////////////////
+void kijelzo(){
+  lcd.setBacklight(255);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("csiraztato hom: ");
+  lcd.print(csir_homerseklet);
+  lcd.setCursor(0, 1);
+  lcd.print("utoljara aktiv: ");
+  lcd.print(csir_last_on);
+  lcd.print(" mp-e");
+
+}
+
+void alarm(String message){
+  Serial.println("alarm: " + message);
+  lcd.setBacklight(255);
+  lcd.clear();
+  lcd.setCursor(0, 3);
+  lcd.print("!JUJUJ!");
+  lcd.setCursor(0, 4);
+  lcd.print(message);
+  lcd.setBacklight(0);
+  delay(400);
+  lcd.setBacklight(255);
+  delay(1000);
+  lcd.setBacklight(0);
+  delay(400);
+  lcd.setBacklight(255);
+  lcd.setBacklight(0);
+  delay(400);
+  lcd.setBacklight(255);
+
+}
+
+////////////KIJELZO///////////////////////
+/////////////////////////////////////////////
+
+/////////////////////////////////////////////
+////////////HTTPUPDATE////////////////////////
+void updateFunc(String Name, String Version)
+{
+  HTTPClient http;
+
+  String url = update_server + "/check?" + "name=" + Name + "&ver=" + Version;
+  Serial.print("[HTTP] check at " + url);
+  if (http.begin(client, url))
+  { // HTTP
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+    delay(10000); //wait for bootup of the server
+    httpCode = http.GET();
+    // httpCode will be negative on error
+    if (httpCode > 0)
+    {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+      {
+        String payload = http.getString();
+        Serial.println(payload);
+        if (payload.indexOf("bin") > 0)
+        {
+          httpUpdateFunc(update_server + payload);
+        }
+      }
+    }
+    else
+    {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+  }
+}
+
+void httpUpdateFunc(String update_url)
+{
+  if ((WiFiMulti.run() == WL_CONNECTED))
+  {
+
+    // The line below is optional. It can be used to blink the LED on the board during flashing
+    // The LED will be on during download of one buffer of data from the network. The LED will
+    // be off during writing that buffer to flash
+    // On a good connection the LED should flash regularly. On a bad connection the LED will be
+    // on much longer than it will be off. Other pins than LED_BUILTIN may be used. The second
+    // value is used to put the LED on. If the LED is on with HIGH, that value should be passed
+    ESPhttpUpdate.setLedPin(LED_BUILTIN, LOW);
+
+    // Add optional callback notifiers
+    ESPhttpUpdate.onStart(update_started);
+    ESPhttpUpdate.onEnd(update_finished);
+    ESPhttpUpdate.onProgress(update_progress);
+    ESPhttpUpdate.onError(update_error);
+
+    t_httpUpdate_return ret = ESPhttpUpdate.update(client, update_url);
+    // Or:
+    //t_httpUpdate_return ret = ESPhttpUpdate.update(client, "server", 80, "file.bin");
+
+    switch (ret)
+    {
+    case HTTP_UPDATE_FAILED:
+      USE_SERIAL.printf("HTTP_UPDATE_FAILD Error (%d): %s\n", ESPhttpUpdate.getLastError(), ESPhttpUpdate.getLastErrorString().c_str());
+      break;
+
+    case HTTP_UPDATE_NO_UPDATES:
+      USE_SERIAL.println("HTTP_UPDATE_NO_UPDATES");
+      break;
+
+    case HTTP_UPDATE_OK:
+      USE_SERIAL.println("HTTP_UPDATE_OK");
+      break;
+    }
+  }
+}
+
+void update_started()
+{
+  USE_SERIAL.println("CALLBACK:  HTTP update process started");
+}
+
+void update_finished()
+{
+  USE_SERIAL.println("CALLBACK:  HTTP update process finished");
+}
+
+void update_progress(int cur, int total)
+{
+  USE_SERIAL.printf("CALLBACK:  HTTP update process at %d of %d bytes...\n", cur, total);
+}
+
+void update_error(int err)
+{
+  USE_SERIAL.printf("CALLBACK:  HTTP update fatal error code %d\n", err);
+}
+////////////HTTPUPDATE////////////////////////
+/////////////////////////////////////////////
+
+//////////////////////////////////////////////
+////////////HTTP  ////////////////////////////
+#include <ESP8266httpUpdate.h>
+#include <ESP8266HTTPClient.h>
+
+void GsheetPost(String sheet_name, String datastring)
+{
+  Serial.println(F("POST to spreadsheet:"));
+  String url = String(F("https://script.google.com/macros/s/")) + String(GScriptId) + "/exec";
+  String payload = String("{\"command\": \"appendRow\", \  \"sheet_name\": \"") + sheet_name + "\", \ \"values\": " + "\"" + datastring + "\"}";
+
+  Serial.println(POSTTask(url, Googlefingerprint, payload));
+};
+
+void discordPost(String message)
+{
+
+  String payload = "{\"content\": \"" + message + "\"}";
+  String url = discord_chanel;
+  Serial.println(POSTTask(url, Discordfingerprint, payload));
+};
+
+String GETTask(String url, const uint8_t Fingeprint[20], uint32_t neededHeap)
+{
+  if (ESP.getFreeHeap() < neededHeap)
+  {
+    Serial.println(F("too few heap left"));
+    discordPost("too few heap:" + String(ESP.getFreeHeap()));
+    ESP.reset();
+  }
+  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
+  HTTPClient https;
+  https.setFollowRedirects(true);
+  if (https.begin(*client, url))
+  {
+    Serial.print(F("[HTTPS] GET "));
+    Serial.println(url);
+    HeapPrintTask();
+
+    int httpCode = https.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0)
+    {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+      if (httpCode == 302)
+      {
+        String redirectUrl = https.getLocation();
+        https.end();
+        return redirectUrl;
+      }
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+      {
+        String payload = https.getString();
+        Serial.println(payload);
+        https.end();
+
+        return payload;
+      }
+    }
+    else
+    {
+      Serial.print(F("[HTTPS] GET... failed, error: "));
+      Serial.println(httpCode);
+      https.end();
+      return "";
+    }
+
+    https.end();
+  }
+  else
+  {
+    Serial.println(F("[HTTPS] Unable to connect"));
+    return "";
+  }
+  return "";
+};
+
+String POSTTask(String url, const uint8_t Fingeprint[20], String payload)
+{
+  std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+  client->setInsecure();
+  HTTPClient https;
+  if (https.begin(*client, url))
+  {
+    Serial.print(F("[HTTPS] POST "));
+    Serial.print(url);
+    Serial.print(" --> ");
+    Serial.println(payload);
+    https.addHeader(F("Content-Type"), F("application/json"));
+    https.setFollowRedirects(true);
+
+    int httpCode = https.POST(payload);
+
+    // httpCode will be negative on error
+    if (httpCode > 0)
+    {
+      // HTTP header has been send and Server response header has been handled
+      Serial.print(F("[HTTPS] POST... code: "));
+      Serial.println(httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
+      {
+        String payload = https.getString();
+        https.end();
+        return payload;
+      }
+    }
+    else
+    {
+      Serial.print(F("[HTTPS] POST... failed, error: "));
+      Serial.println(httpCode);
+      https.end();
+      return "";
+    }
+
+    https.end();
+  }
+  else
+  {
+    Serial.println(F("[HTTPS] Unable to connect"));
+    return "";
+  }
+  return "";
+};
+
+////////////HTTP  ////////////////////////////
+//////////////////////////////////////////////
+
+//////////////////////////////////////////////
+////////////GETCONFIG/////////////////////////
+
+const size_t capacity = JSON_OBJECT_SIZE(4) + 60;
+DynamicJsonDocument doc(capacity);
+
+void getdata()
+{
+  String baseurl = String(F("https://script.google.com/macros/s/")) + String(GScriptId) + "/exec?";
+
+  String params = "csir_homerseklet=0&csir_last_on=0"; //ezt piffmanből a legegyszerűbb
+
+  String url = baseurl + params;
+  String response = GETTask(url, Googlefingerprint, 1000);
+  network_timeout++;
+  if (response.length() > 1)
+  {
+    network_timeout=0;
+    deserializeJson(doc, response);
+
+   csir_homerseklet = doc["csir_homerseklet"];
+  csir_last_on = doc["csir_last_on"];
+
+    Serial.println("data got: csir_homerseklet =" + String(csir_homerseklet));
+  }
+}
+
+void getconfig()
+{
+  String baseurl = String(F("https://script.google.com/macros/s/")) + String(GScriptId) + "/exec?";
+
+  String params = "update_interval=0&pinginterval=0&csir_timeout=0"; //ezt piffmanből a legegyszerűbb
+
+  String url = baseurl + params;
+  String response = GETTask(url, Googlefingerprint, 1000);
+
+  if (response.length() > 1)
+  {
+    deserializeJson(doc, response);
+
+    pinginterval = doc["pinginterval"];
+    update_interval = doc["update_interval"];
+    csir_timeout = doc["csir_timeout"];
+
+    Serial.println("Config got: pinginterval =" + String(pinginterval));
+  }
+}
+
+////////////GETCONFIG/////////////////////////
+//////////////////////////////////////////////
+void HeapPrintTask()
+{
+  /* lcd.setCursor(0, 2);
+    lcd.print("                                        ");
+    lcd.setCursor(0, 2);
+    lcd.print("Free heap: ");
+    lcd.print(ESP.getFreeHeap());*/
+
+  // we could use getFreeHeap() getMaxFreeBlockSize() and getHeapFragmentation()
+  // or all at once:
+  uint32_t free;
+  uint16_t max;
+  uint8_t frag;
+  ESP.getHeapStats(&free, &max, &frag);
+  float freepercent = free;
+  freepercent = freepercent / 81920 * 100;
+  Serial.printf("free: %5d - max: %5d - freepercent: %2.1f%% - frag: %3d%% <- ", free, max, freepercent, frag);
+  Serial.println();
+}
